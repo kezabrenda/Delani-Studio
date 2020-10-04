0718a32aba4d15a6379e874db6711eb650fac0c5
keza1brenda <keza1brenda@gmail.com>
2020-10-04 12:46:14 +0200

    add services style to jquery

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+
+$("#design").click(function(){
+    $("#showDesign").toggle();
+    $(".noShowDsgn").toggle();
+  });
+  
+  
+  $("#dvlpnt").click(function(){
+    $("#showDvlpnt").toggle();
+    $(".noShowDvlpnt").toggle();
+  });
+
+
+  $("#product").click(function(){
+    $("#showPrdct").toggle();
+    $(".noShowPrdct").toggle();
+  });
+
+})
